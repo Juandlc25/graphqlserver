c6db56853a712ad54cf1e33f8039b9cdac3e47dc
JuandlcJrDev <jdelacruz@hellobuild.co>
2020-12-03 18:37:21 -0500

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://juandlc:mJXvl9C7QY8Y67jz@cluster0.3rhox.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "kjjahdlfjhasdjfhlsdkhflajskdhf",
+};
